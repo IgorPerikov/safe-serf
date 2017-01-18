b9824ded50f05a5a24c7c80cfe7ac211bdaade8b
IgorPerikov <perikov.igor@gmail.com>
2017-01-18 22:54:25 +0300

    complete cassandra configuration

--- api/src/main/java/org/clayman/safe/api/configuration/CassandraConfiguration.java
+++ api/src/main/java/org/clayman/safe/api/configuration/CassandraConfiguration.java
@@ -1,14 +1,34 @@
 package org.clayman.safe.api.configuration;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ProtocolOptions;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.mapping.MappingManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.InetSocketAddress;
 
 @Configuration
 public class CassandraConfiguration {
 
-    @Bean
-    public Session session() {
+    public static final int CASSANDRA_DEFAULT_PORT = 9042;
 
+    @Bean(destroyMethod = "close")
+    public Cluster cluster() {
+        Cluster.Builder clusterBuilder = Cluster.builder()
+                .addContactPointsWithPorts(new InetSocketAddress("localhost", CASSANDRA_DEFAULT_PORT))
+                .withCompression(ProtocolOptions.Compression.LZ4);
+        return clusterBuilder.build();
+    }
+
+    @Bean(destroyMethod = "close")
+    public Session session(Cluster cluster) {
+        return cluster.connect();
+    }
+
+    @Bean
+    public MappingManager mappingManager(Session session) {
+        return new MappingManager(session);
     }
 }
--- api/src/main/java/org/clayman/safe/api/web/controller/OrderController.java
+++ api/src/main/java/org/clayman/safe/api/web/controller/OrderController.java
@@ -1,7 +1,11 @@
 package org.clayman.safe.api.web.controller;
 
+import org.clayman.safe.api.entity.Order;
 import org.clayman.safe.api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,9 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Order createOrder(@RequestBody Order order) {
+        return null;
+    }
 }
