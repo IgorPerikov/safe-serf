707a3d810583c1265a9cb806b5cc0a3935de58ba
IgorPerikov <perikov.igor@gmail.com>
2017-01-28 20:57:26 +0300

    add logging for api

--- api/src/main/java/org/clayman/safe/api/service/OrderService.java
+++ api/src/main/java/org/clayman/safe/api/service/OrderService.java
@@ -3,6 +3,8 @@ package org.clayman.safe.api.service;
 import org.clayman.safe.api.entity.Order;
 import org.clayman.safe.api.repository.OrderRepository;
 import org.clayman.safe.api.service.kafka.OrderMessagingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.UUID;
 
 @Service
 public class OrderService {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
 
     @Autowired
     private OrderMessagingService orderMessagingService;
@@ -19,6 +23,7 @@ public class OrderService {
 
     public Order acceptOrder(Order order) {
         order.setId(UUID.randomUUID());
+        log.info("Got new order, set id to {}", order.getId().toString());
         handleNewOrder(order);
         return order;
     }
--- api/src/main/java/org/clayman/safe/api/service/kafka/OrderMessagingService.java
+++ api/src/main/java/org/clayman/safe/api/service/kafka/OrderMessagingService.java
@@ -3,11 +3,15 @@ package org.clayman.safe.api.service.kafka;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.clayman.safe.api.entity.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderMessagingService {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderMessagingService.class);
 
     private static final String TOPIC_NAME = "orders";
 
@@ -16,5 +20,6 @@ public class OrderMessagingService {
 
     public void sendNewOrderInfo(Order order) {
         producer.send(new ProducerRecord<>(TOPIC_NAME, order.getId().toString(), order.getUrl()));
+        log.info("Successfully send order to kafka with id={}", order.getId().toString());
     }
 }
